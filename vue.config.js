e34cb5fa861a80e5890dda780e7bf1265c59ae01
Tanoooki <schritke@hotmail.com>
2023-09-29 17:53:24 -0300

    teste 3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/indiebrew",
 })
+
+module.exports = {
+  publicPath: "/indiebrew2/",
+}
